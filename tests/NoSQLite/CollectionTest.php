79effeab261a1a233f2f2946845dfc5950a006b5
Maciej Winnicki <maciej.winnicki@gmail.com>
2011-07-24 18:57:05 +0200

    Test pack

---
+++ tests/NoSQLite/CollectionTest.php
@@ -0,0 +1,81 @@
+<?php
+require_once '../NoSQLite.php';
+require_once '../NoSQLite/Collection.php';
+
+class CollectionTest extends PHPUnit_Framework_TestCase
+{
+    const DB_FILE = 'testCollectionTest.db';
+
+    protected $_nsl;
+    protected $_collection;
+
+    public function setUp()
+    {
+        $this->_nsl = new NoSQLite\NoSQLite(self::DB_FILE);
+        $this->_collection = $this->_nsl->getCollection('test');
+    }
+
+    /**
+     * @dataProvider validData
+     */
+    public function testSetGetValue($key, $value)
+    {
+        $this->_collection->set($key, $value);
+        $this->assertEquals($this->_collection->get($key), $value);
+    }
+
+    /**
+     * @dataProvider inValidData
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetGetExceptions($key, $value)
+    {
+        $this->_collection->set($key, $value);
+        $this->_collection->get($key);
+    }
+
+    public function testDeleteAll()
+    {
+        $this->_collection->set(uniqid(), 'value');
+        $this->_collection->deleteAll();
+        $this->assertEquals(array(), $this->_collection->getAll());
+    }
+
+    public function testGetAll()
+    {
+        $data = array(
+            '_1' => 'value1',
+            '_2' => 'value2'
+        );
+
+        $this->_collection->deleteAll();
+
+        foreach($data as $key => $value)
+        {
+            $this->_collection->set($key, $value);
+        }
+
+        $this->assertEquals($data, $this->_collection->getAll());
+    }
+
+    public static function validData()
+    {
+        return array(
+            array('key', 'value'),
+            array('0', 'value'),
+        );
+    }
+
+    public static function inValidData()
+    {
+        return array(
+            array(0, 'value'),
+            array('key', 0)
+        );
+    }
+
+    public function tearDown()
+    {
+        unlink(self::DB_FILE);
+    }
+}
---
+++ tests/NoSQLiteTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../NoSQLite.php';
+
+class NoSQLiteTest extends PHPUnit_Framework_TestCase
+{
+    const DB_FILE = 'testNoSQLiteTest.db';
+
+    public function testNewCollection()
+    {
+        $nsl = new NoSQLite\NoSQLite(self::DB_FILE);
+        $collection = $nsl->getCollection('test');
+        $this->assertEquals(get_class($collection), 'NoSQLite\Collection');
+    }
+
+    public function __destruct()
+    {
+        unlink(self::DB_FILE);
+    }
+}
